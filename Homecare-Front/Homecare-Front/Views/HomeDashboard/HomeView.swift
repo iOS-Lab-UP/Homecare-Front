bacceadd74d4737d670083b164dd5d755c8bb5d9
luisced <luis.cedillo.maldonado@gmail.com>
2024-02-06 18:03:26 -0600

    feat: restructed the project

--- Homecare-Front/Homecare-Front/Views/HomeDashboard/HomeView.swift
+++ Homecare-Front/Homecare-Front/Views/HomeDashboard/HomeView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Spacer()
         VStack {
@@ -33,5 +33,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
